0c60693267bf09685153f416bf188e952f09b769
David Chin <david.w.h.chin@gmail.com>
2013-04-15 12:48:34 -0400

    Updated to latest version

--- twitter/oauth.py
+++ twitter/oauth.py
@@ -1,11 +1,61 @@
-from twitter.auth import Auth
+"""
+Visit the Twitter developer page and create a new application:
+
+    https://dev.twitter.com/apps/new
+
+This will get you a CONSUMER_KEY and CONSUMER_SECRET.
+
+When users run your application they have to authenticate your app
+with their Twitter account. A few HTTP calls to twitter are required
+to do this. Please see the twitter.oauth_dance module to see how this
+is done. If you are making a command-line app, you can use the
+oauth_dance() function directly.
+
+Performing the "oauth dance" gets you an ouath token and oauth secret
+that authenticate the user with Twitter. You should save these for
+later so that the user doesn't have to do the oauth dance again.
+
+read_token_file and write_token_file are utility methods to read and
+write OAuth token and secret key values. The values are stored as
+strings in the file. Not terribly exciting.
+
+Finally, you can use the OAuth authenticator to connect to Twitter. In
+code it all goes like this::
+
+    MY_TWITTER_CREDS = os.path.expanduser('~/.my_app_credentials')
+    if not os.path.exists(MY_TWITTER_CREDS):
+        oauth_dance("My App Name", CONSUMER_KEY, CONSUMER_SECRET,
+                    MY_TWITTER_CREDS)
+
+    oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
+
+    twitter = Twitter(auth=OAuth(
+        oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET))
+
+    # Now work with Twitter
+    twitter.statuses.update(status='Hello, world!')
+
+"""
+
+from __future__ import print_function
 
 from time import time
 from random import getrandbits
-from time import time
-import urllib
+
+try:
+    import urllib.parse as urllib_parse
+    from urllib.parse import urlencode
+    PY3 = True
+except ImportError:
+    import urllib2 as urllib_parse
+    from urllib import urlencode
+    PY3 = False
+
 import hashlib
 import hmac
+import base64
+
+from .auth import Auth
 
 
 def write_token_file(filename, oauth_token, oauth_token_secret):
@@ -13,8 +63,8 @@ def write_token_file(filename, oauth_token, oauth_token_secret):
     Write a token file to hold the oauth token and oauth token secret.
     """
     oauth_file = open(filename, 'w')
-    print >> oauth_file, oauth_token
-    print >> oauth_file, oauth_token_secret
+    print(oauth_token, file=oauth_file)
+    print(oauth_token_secret, file=oauth_file)
     oauth_file.close()
 
 def read_token_file(filename):
@@ -52,16 +102,17 @@ class OAuth(Auth):
         params['oauth_timestamp'] = str(int(time()))
         params['oauth_nonce'] = str(getrandbits(64))
 
-        enc_params = urlencode_noplus(sorted(params.iteritems()))
+        enc_params = urlencode_noplus(sorted(params.items()))
 
-        key = self.consumer_secret + "&" + urllib.quote(self.token_secret, '')
+        key = self.consumer_secret + "&" + urllib_parse.quote(self.token_secret, safe='~')
 
         message = '&'.join(
-            urllib.quote(i, '') for i in [method.upper(), base_url, enc_params])
+            urllib_parse.quote(i, safe='~') for i in [method.upper(), base_url, enc_params])
 
-        signature = hmac.new(
-            key, message, hashlib.sha1).digest().encode('base64')[:-1]
-        return enc_params + "&" + "oauth_signature=" + urllib.quote(signature, '')
+        signature = (base64.b64encode(hmac.new(
+                    key.encode('ascii'), message.encode('ascii'), hashlib.sha1)
+                                      .digest()))
+        return enc_params + "&" + "oauth_signature=" + urllib_parse.quote(signature, safe='~')
 
     def generate_headers(self):
         return {}
@@ -71,20 +122,13 @@ class OAuth(Auth):
 # also in the request itself.)
 # So here is a specialized version which does exactly that.
 def urlencode_noplus(query):
-    if hasattr(query,"items"):
-        # mapping objects
-        query = query.items()
+    if not PY3:
+        new_query = []
+        for k,v in query:
+            if type(k) is unicode: k = k.encode('utf-8')
+            if type(v) is unicode: v = v.encode('utf-8')
+            new_query.append((k, v))
+        query = new_query
+        return urlencode(query).replace("+", "%20")
 
-    encoded_bits = []
-    for n, v in query:
-        # and do unicode here while we are at it...
-        if isinstance(n, unicode):
-            n = n.encode('utf-8')
-        else:
-            n = str(n)
-        if isinstance(v, unicode):
-            v = v.encode('utf-8')
-        else:
-            v = str(v)
-        encoded_bits.append("%s=%s" % (urllib.quote(n, ""), urllib.quote(v, "")))
-    return "&".join(encoded_bits)
+    return urlencode(query, safe='~').replace("+", "%20")
--- twitter/oauth_dance.py
+++ twitter/oauth_dance.py
@@ -1,9 +1,18 @@
 
+from __future__ import print_function
+
 import webbrowser
 import time
 
-from api import Twitter
-from oauth import OAuth, write_token_file
+from .api import Twitter
+from .oauth import OAuth, write_token_file
+
+try:
+    _input = raw_input
+except NameError:
+    _input = input
+
+
 
 def oauth_dance(app_name, consumer_key, consumer_secret, token_filename=None):
     """
@@ -12,25 +21,27 @@ def oauth_dance(app_name, consumer_key, consumer_secret, token_filename=None):
 
     Provide the name of your app in `app_name`, your consumer_key, and
     consumer_secret. This function will open a web browser to let the
-    user Allow your app to access their Twitter account. PIN
+    user allow your app to access their Twitter account. PIN
     authentication is used.
 
     If a token_filename is given, the oauth tokens will be written to
     the file.
     """
-    print ("Hi there! We're gonna get you all set up to use %s." % app_name)
+    print("Hi there! We're gonna get you all set up to use %s." % app_name)
     twitter = Twitter(
         auth=OAuth('', '', consumer_key, consumer_secret),
-        format='')
+        format='', api_version=None)
     oauth_token, oauth_token_secret = parse_oauth_tokens(
         twitter.oauth.request_token())
-    print """
+    print("""
 In the web browser window that opens please choose to Allow
 access. Copy the PIN number that appears on the next page and paste or
 type it here:
-"""
+""")
     oauth_url = ('http://api.twitter.com/oauth/authorize?oauth_token=' +
                  oauth_token)
+    print("Opening: %s\n" % oauth_url)
+
     try:
         r = webbrowser.open(oauth_url)
         time.sleep(2) # Sometimes the last command can print some
@@ -39,24 +50,24 @@ type it here:
         if not r:
             raise Exception()
     except:
-        print """
+        print("""
 Uh, I couldn't open a browser on your computer. Please go here to get
 your PIN:
 
-""" + oauth_url
-    oauth_verifier = raw_input("Please enter the PIN: ").strip()
+""" + oauth_url)
+    oauth_verifier = _input("Please enter the PIN: ").strip()
     twitter = Twitter(
         auth=OAuth(
             oauth_token, oauth_token_secret, consumer_key, consumer_secret),
-        format='')
+        format='', api_version=None)
     oauth_token, oauth_token_secret = parse_oauth_tokens(
         twitter.oauth.access_token(oauth_verifier=oauth_verifier))
     if token_filename:
         write_token_file(
             token_filename, oauth_token, oauth_token_secret)
-        print
-        print "That's it! Your authorization keys have been written to %s." % (
-            token_filename)
+        print()
+        print("That's it! Your authorization keys have been written to %s." % (
+            token_filename))
     return oauth_token, oauth_token_secret
 
 def parse_oauth_tokens(result):
